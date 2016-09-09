d60be085c90a612de6972c7271ff5e4a2484050c
rpadebet <rohit.pittu@gmail.com>
2016-09-09 13:05:47 -0400

    Committing Code and generated plots for Week 1 Project

---
+++ plot2.R
@@ -0,0 +1,26 @@
+# Loading and Preparing Data
+h_pow_df<-read.table("household_power_consumption.txt",
+                     sep = ";",stringsAsFactors = FALSE,
+                     na.strings = "?",header = TRUE
+)
+# Subset for the 2 days we are interested in
+h_pow_sub <- subset(h_pow_df,
+                    as.Date(h_pow_df$Date,"%d/%m/%Y") >= as.Date("2007-02-01","%Y-%m-%d") 
+                    &
+                        as.Date(h_pow_df$Date,"%d/%m/%Y") <= as.Date("2007-02-02","%Y-%m-%d") )
+# Create a new datetime column to plot against
+h_pow_sub$DateTime <- paste(h_pow_sub$Date ,h_pow_sub$Time)
+# Convert the date time column to POSIXlt class
+h_pow_sub$DateTime<-strptime(h_pow_sub$DateTime,"%d/%m/%Y %H:%M:%S")
+
+# Opening PNG File
+png(filename = "plot2.png", width = 480, height = 480)
+
+### Plot 2
+plot(x = h_pow_sub$DateTime,y = h_pow_sub$Global_active_power,
+     type = "l",
+     ylab = "Global Active Power (kilowatts)",
+     xlab = "")
+
+
+dev.off()
